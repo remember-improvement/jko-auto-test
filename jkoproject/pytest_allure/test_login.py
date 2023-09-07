dccce26cbf4a20b76debc538adddd9a12d9c4f95
Nick Lee <nick_lee@adata.com>
2023-09-08 01:23:43 +0800

    modify status and message

--- jkoproject/pytest_allure/test_login.py
+++ jkoproject/pytest_allure/test_login.py
@@ -12,9 +12,8 @@ test_data = [
             "LoginAuth": "Aa12345678",
             "DateTime": "2023-08-10T12:00:00",
         },
-        "Success",
-        "Login successful",
         200,
+        "Login successful",
     ),
     (
         "Login Failed with incorrect username",
@@ -23,9 +22,8 @@ test_data = [
             "LoginAuth": "Aa12345678",
             "DateTime": "2023-08-10T12:00:00",
         },
-        "ErrorCode_03",
-        "XXX_03",
         401,
+        "Login failed",
     ),
     (
         "Login Failed with incorrect loginAuth",
@@ -34,48 +32,42 @@ test_data = [
             "LoginAuth": "0000",
             "DateTime": "2023-08-10T12:00:00",
         },
-        "ErrorCode_03",
-        "XXX_03",
         401,
+        "Login failed",
     ),
     (
-        "Login Failed without username",
+        "Login Failed without account",
         {"LoginAuth": "0000", "DateTime": "2023-08-10T12:00:00"},
-        "ErrorCode_01",
-        "XXX_01",
         400,
+        "Account is required",
     ),
     (
         "Login Failed without loginAuth",
         {"Account": "currentuser", "DateTime": "2023-08-10T12:00:00"},
-        "ErrorCode_01",
-        "XXX_01",
         400,
+        "LoginAuth is required",
     ),
     (
         "Login Successful without DateTime",
         {"Account": "correctuser", "LoginAuth": "Aa12345678"},
-        "Success",
-        "Login successful",
         200,
+        "Login successful",
     ),
     (
         "Login Successful with incorrect DateTime",
         {"Account": "correctuser", "LoginAuth": "Aa12345678", "DateTime": "123"},
-        "Success",
-        "Login successful",
         200,
+        "Login successful",
     ),
     (
-        "Login Failed with long username",
+        "Login Failed with long account",
         {
             "Account": "currentuser1234567890",
             "LoginAuth": "Aa12345678",
             "DateTime": "2023-08-10T12:00:00",
         },
-        "ErrorCode_02",
-        "XXX_02",
         400,
+        "Account length too long",
     ),
     (
         "Login Failed with long loginAuth",
@@ -84,9 +76,8 @@ test_data = [
             "LoginAuth": "Aa12345678Aa12345678Aa123456787654321",
             "DateTime": "2023-08-10T12:00:00",
         },
-        "ErrorCode_02",
-        "XXX_02",
         400,
+        "LoginAuth length too long",
     ),
     (
         "Login Failed with long DateTime",
@@ -95,17 +86,10 @@ test_data = [
             "LoginAuth": "Aa12345678",
             "DateTime": "2023-08-10T12:00:00xx",
         },
-        "ErrorCode_02",
-        "XXX_02",
         400,
+        "DateTime length too long",
     ),
-    (
-        "Login Failed without body",
-        None,
-        "ErrorCode_05",
-        "XXX_05",
-        400,
-    ),
+    ("Login Failed without body", None, 400, "request body is required"),
     (
         "Login Failed with invalid request method",
         {
@@ -113,19 +97,18 @@ test_data = [
             "LoginAuth": "Aa12345678",
             "DateTime": "2023-08-10T12:00:00",
         },
-        "ErrorCode_04",
-        "XXX_04",
         405,
+        "Method should be POST",
     ),
 ]
 
 
 @allure.feature("Login API Tests")
 @pytest.mark.parametrize(
-    "title, payload, status, message, status_code",
+    "title, payload, status, message",
     test_data,
 )
-def test_login_api(title, payload, status, message, status_code):
+def test_login_api(title, payload, status, message):
     allure.dynamic.title(title)
     body = payload
     if title == "Login Failed with invalid request method":
@@ -136,6 +119,6 @@ def test_login_api(title, payload, status, message, status_code):
             response = requests.post(API_URL, json=body)
     with allure.step("Verifying response status code and content"):
         res_json = response.json()
-        assert response.status_code == status_code
+        assert response.status_code == status
         assert res_json["Status"] == status
         assert res_json["Message"] == message
